38e5e670802546fc6f82246cb6178df241b2b3dd
JosephJvB <joevanbo@pm.me>
2019-01-13 22:20:17 +1300

    basic header row, content live on popup island

--- client/main.vue
+++ client/main.vue
@@ -1,40 +1,24 @@
 <template>
 <b-container class="j-container" fluid> <!-- always 100% view width -->
-  <b-row class="header">
-    <div class="w-100 d-flex">
-      <h4 class="col-8">Currently logged in as: {{currentUsername || 'Guest'}}</h4>
-      <button class="col-4">Hamburger here?</button>
+  <b-row class="header py-3">
+    <div class="col-2 d-flex">
+      <h4 class="m-auto">As:</h4>
+      <h4 class="m-auto username">{{currentUsername || 'Guest'}}</h4>
     </div>
-    <div class="w-100 d-flex py-1">
-      <div class="col d-flex justify-center">
-        <input class="main-search" type="text" placeholder="search"/>
-        <i class="material-icons">search</i>
-      </div>
+    <div class="col-8 d-flex mx-auto justify-content-center">
+      <input class="main-search" type="text" placeholder="search"/>
+      <i class="material-icons">search</i>
+    </div>
+    <div class="col-2 d-flex justify-content-end">
+      <i class="material-icons">reorder</i>
     </div>
   </b-row>
   <!-- main content -->
   <b-row class="main">
-    <!-- spacer -->
-    <div class="col-md-1"></div>
-    <!-- content -->
-    <div class="col-md-7 content">
-      <h1>Home</h1>
-      <div class="row item" v-for="(item, index) in content" :key="index">
-        <img src="https://via.placeholder.com/150" alt="holder">
-        {{item}}
-      </div>
+    <div class="col-xs-12 col-md-10 col-lg-8 content">
+      <!-- modals -->
+      <router-view/>
     </div>
-    <!-- right-hand sidebar -->
-    <div class="col-md-3 sidebar">
-      <div class="row item" v-for="(item, index) in sidebar" :key="index">
-        <img src="https://via.placeholder.com/50" alt="holder">
-        {{item}}
-      </div>
-    </div>
-    <!-- spacer -->
-    <div class="col-md-1"></div>
-    <!-- modals -->
-    <router-view/>
   </b-row>
 </b-container>
 </template>
@@ -45,9 +29,9 @@ import { mapGetters, mapActions } from 'vuex'
 export default {
   data() {
     return {
-      content: [1, 2, 3, 4],
-      sidebar: [1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
+  },
+  methods: {
   },
   computed: {
     ...mapGetters(['currentUsername'])
@@ -56,29 +40,47 @@ export default {
 </script>
 
 <style>
+/* box-shadow: h v blur spread color; */
   .j-container {
     height:100%
   }
   .header {
     background-color: grey;
+    box-shadow: 0 1px 2px 1px #383535;
   }
-  .main {
-    background-color: #6c757d;
-  }
-  .item {
-    border: solid 1px #ffc107;
+  .username:hover {
+    color: #ffc107;
+    cursor: pointer;
+    text-decoration: underline;
   }
   .main-search {
-    width: 95%;
+    background-color: lightyellow;
+    width: 70%;
+    margin-right: 5px;
+    padding: 0 2rem;
+    box-shadow: -3px 1px 5px 1px inset #383535;
   }
   .material-icons {
-    margin-top: 2px;
+    margin: auto 0;
     padding: 10px;
-    border: solid 1px transparent;
+    border-radius: 1000px;
+    box-shadow: 1px 1px 5px 1px #383535;
+    max-width: 45px;
+    max-height: 45px;
   }
   .material-icons:hover {
     color: #ffc107;
-    border: solid 1px #ffc107;
-    border-radius: 1000px;
+    cursor: pointer;
+  }
+  .main {
+    border-top: solid 1px #5f5858;
+    background-color: #6c757d;
+    padding-top: 2rem;
+  }
+  .content {
+    margin: auto;
+    padding-top: 1rem;
+    height: 70vh;
+    box-shadow: 2px 2px 10px 3px #383535;
   }
 </style>
